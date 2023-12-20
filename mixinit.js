85470dfaa23f5ca1dc0120a90178884b2dc4b85f
moosedrool <5903100+moosedrool@users.noreply.github.com>
2023-12-20 08:10:50 -0800

    Update mixinit.js

--- mixinit.js
+++ mixinit.js
@@ -46,7 +46,7 @@ mixpanel.init(MIXPANEL_PROJECT_TOKEN, {
 
       
         var pageTitle = document.title;
-		console.log("The title of the page is: " + pageTitle);
+		console.log("The T I T L E of the page is: " + pageTitle);
       	var currentDomain = window.location.hostname;
 		console.log("Current domain: " + currentDomain);
       	var currentPath = window.location.pathname;
@@ -86,6 +86,8 @@ if (document.readyState === "loading") {
 
 function initializeEventListeners() {
     console.log("DOM Loaded");
+
+    // Event listener for contact class
     document.querySelectorAll('.contact:not(div)').forEach(item => {
         item.addEventListener('click', event => {
             console.log("SUCESS");
@@ -116,7 +118,65 @@ function initializeEventListeners() {
 
 function handleSubmitButtonClick(event) {
     console.log("Submit button clicked");
-    // Logic for handling submit button click
-    // ...
+    var emailValue = document.getElementById('input_1').value;  // Capture email value
+    var newsletterSubscription = document.getElementById('input_5_0').checked;  // Capture checkbox state
+    var serviceLine = document.getElementById('input_6').value;
+    var urgent = document.getElementById('input_7_0').checked; 
+    var phone_number = document.getElementById('input_11').value;
+
+    var dataObject = {
+      $email: emailValue,  // Structure data into JSON object
+      newsletter: newsletterSubscription,  // Add newsletter subscription status to JSON object
+      serviceLine: serviceLine
+    };
+    
+    var pageTitle = document.title;
+    console.log("The title of the page is: " + pageTitle);
+    var currentDomain = window.location.hostname;
+    console.log("Current domain: " + currentDomain);
+    var currentPath = window.location.pathname;
+    console.log("Current path: " + currentPath);
+    var currentProtocol = window.location.protocol;
+    console.log("Current protocol: " + currentProtocol);
+    var queryString = window.location.search;
+    console.log("Query string: " + queryString);
+
+      //window.addEventListener('mpEZTrackLoaded', ()=>{
+      // mixpanel.ez is always available in this scope
+      mixpanel.identify(emailValue);
+      mixpanel.people.set(dataObject);
+
+      //mixpanel.ez.people.set({$email: emailValue});
+      //mixpanel.ez.people.set({newsletter: newsletterSubscription});
+      //mixpanel.ez.people.set({serviceLine: serviceLine});
+
+      mixpanel.track('Contact', 
+                     {"Channel": "Appointment request",
+                      "Service Line": serviceLine, 
+                      "Urgent": urgent,
+                      "Phone Number": phone_number,
+                      "$email" : emailValue,
+                      "current_page_title":   pageTitle,
+                      "current_domain":   currentDomain,
+                      "current_url_path":   currentPath,
+                      "current_url_protocol": currentProtocol,
+                      "current_url_search": queryString       
+                      });
+                      
+    if (newsletterSubscription) {
+    console.log("Checkbox is checked. Doing some stuff...");
+    
+         mixpanel.track('Newsletter Sign Up', {
+            'Service Line': serviceLine, 
+            'newsletter': newsletterSubscription,
+            'Phone Number': phone_number,
+            '$email' : emailValue,
+           "current_page_title":  pageTitle,
+           "current_domain":    currentDomain,
+           "current_url_path":    currentPath,
+           "current_url_protocol":  currentProtocol,
+           "current_url_search":  queryString  });
+    }
+	
 }
 </script>
