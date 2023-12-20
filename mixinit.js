fa48fcd6a88bf16aba29decb07437a91c3edfce2
moosedrool <5903100+moosedrool@users.noreply.github.com>
2023-12-20 07:51:38 -0800

    Update mixinit.js

--- mixinit.js
+++ mixinit.js
@@ -46,7 +46,7 @@ mixpanel.init(MIXPANEL_PROJECT_TOKEN, {
 
       
         var pageTitle = document.title;
-		console.log("The TITLE of the page is: " + pageTitle);
+		console.log("The title of the page is: " + pageTitle);
       	var currentDomain = window.location.hostname;
 		console.log("Current domain: " + currentDomain);
       	var currentPath = window.location.pathname;
@@ -86,90 +86,25 @@ if (document.readyState === "loading") {
 
 function initializeEventListeners() {
     console.log("DOM Loaded");
-    document.getElementById('233031288507150').addEventListener('submit', function(event) {
-      //event.preventDefault();  // Prevent the default form submission action
-	
-	    var emailValue = document.getElementById('input_1').value;  // Capture email value
-	    var newsletterSubscription = document.getElementById('input_5_0').checked;  // Capture checkbox state
-	    var serviceLine = document.getElementById('input_6').value;
-	    var urgent = document.getElementById('input_7_0').checked; 
-	    var phone_number = document.getElementById('input_11').value;
-	
-	    var dataObject = {
-	      $email: emailValue, 
-	      newsletter: newsletterSubscription,  
-	      serviceLine: serviceLine
-	    };
-	    
-	    
-	    var pageTitle = document.title;
-			console.log("The TITLE of the page is: " + pageTitle);
-	    var currentDomain = window.location.hostname;
-			console.log("Current domain: " + currentDomain);
-	    var currentPath = window.location.pathname;
-			console.log("Current path: " + currentPath);
-	    var currentProtocol = window.location.protocol;
-			console.log("Current protocol: " + currentProtocol);
-			var queryString = window.location.search;
-			console.log("Query string: " + queryString);
-	
-	      //window.addEventListener('mpEZTrackLoaded', ()=>{
-	      // mixpanel.ez is always available in this scope
-	      mixpanel.identify(emailValue);
-	      mixpanel.people.set(dataObject);
-	
-	      //mixpanel.ez.people.set({$email: emailValue});
-	      //mixpanel.ez.people.set({newsletter: newsletterSubscription});
-	      //mixpanel.ez.people.set({serviceLine: serviceLine});
-	
-	      mixpanel.track('Contact', 
-	                {"Channel": "Appointment request",
-				"Service Line": serviceLine, 
-				"Urgent": urgent,
-				"Phone Number": phone_number,
-				"$email" : emailValue,
-				"current_page_title": 	pageTitle,
-				"current_domain": currentDomain,
-				"current_url_path": currentPath,
-				"current_url_protocol":	currentProtocol,
-				"current_url_search":	queryString       
-	                      });
-	                      
-		if (newsletterSubscription) {
-			console.log("Checkbox is checked. Doing some stuff...");
-		    
-			 mixpanel.track('Newsletter Sign Up', {
-					'Service Line': serviceLine, 
-					'newsletter': newsletterSubscription,
-				    'Phone Number': phone_number,
-					'$email' : emailValue,
-			   "current_page_title": 	pageTitle,
-			   "current_domain":		currentDomain,
-			   "current_url_path":		currentPath,
-			   "current_url_protocol":	currentProtocol,
-			   "current_url_search":	queryString  });
-		}
-	    
-	      setTimeout(() => {
-	        event.target.submit();
-	      }, 1000); // Adjust delay as needed
-
-
-      //})
-      // Convert JSON object to string and format it with indentation
-      // Open a new window and write the JSON string to it
-      //var jsonString = JSON.stringify(dataObject, null, 2);
-      //var newWindow = window.open("", "_blank");
-      //newWindow.document.write(`<pre>${jsonString}</pre>`);
-      //newWindow.document.write(`<pre>${emailValue}</pre>`);
-      //newWindow.document.write(`<pre>${serviceLine}</pre>`);
-      //
-      //return false;  // Prevent the default form submission action, if event.preventDefault() is not sufficient
-
-	    
+    document.querySelectorAll('.contact:not(div)').forEach(item => {
+        item.addEventListener('click', event => {
+            console.log("SUCESS");
+        const clickedElement = event.target; // This is the element that was clicked
+        // Using closest to ensure we get the <a> tag even if the click was on a descendant
+        const actualLinkElement = clickedElement.closest('a');    
+          //console.log("actualLinkElement: ", actualLinkElement);
+          if (actualLinkElement) {
+             // Get the href attribute of the closest <a> element
+            const actualHref = actualLinkElement.getAttribute('href');
+            //console.log(actualHref);
+            mixpanel.track("Contact", {
+                "Channel": actualHref,
+                "URL Path": location.pathname
+            });
+            event.stopPropagation();
+          }
+		
+        });
     });
-});
-
 }
 </script>
-
