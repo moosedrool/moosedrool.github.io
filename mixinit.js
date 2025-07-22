50dc59059ff527995b1430ec37775b5568393a76
moosedrool <5903100+moosedrool@users.noreply.github.com>
2025-07-21 22:55:19 -0700

    Update mixinit.js

--- mixinit.js
+++ mixinit.js
@@ -106,7 +106,7 @@ mixpanel.init(MIXPANEL_PROJECT_TOKEN, {
 <script type="text/javascript">
   window.addEventListener("message", function(event) {
     // Verify the message origin if desired (update the domain as needed)
-    if (event.origin.indexOf("storage.googleapis.com/ga2mxp-static-cdn") === -1) {
+    if (event.origin.indexOf("storage.googleapis.com") === -1) {
       console.warn("Unexpected origin: " + event.origin);
       return;
     }
